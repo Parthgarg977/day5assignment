466a8daeb79df5a54eb6067b09fe7fc9694590db
parthgarg977 <parthgarg977@gmail.com>
2022-08-06 00:52:22 +0530

    2nd problem commit

---
+++ Day5_assign/P2.cs
@@ -0,0 +1,30 @@
+﻿namespace Simple
+{
+    class P2
+    {
+		public void leapYear()
+		{
+
+			Console.WriteLine("Enter year to check: ");
+			int year = Convert.ToInt32(Console.ReadLine());   //	year to be checked
+
+			if ((year / 1000) == 0)
+			{   //	year is not four digit
+				Console.WriteLine("Year should be in 4 digits!!!");
+				leapYear();
+			}
+			else
+			{
+				if ((year % 4) == 0)
+				{   //	leap year
+					Console.WriteLine("Leap year");
+				}
+				else
+				{   //	not a leap year
+					Console.WriteLine("Not a leap year");
+				}
+			}
+
+		}
+	}
+}
--- Day5_assign/Program.cs
+++ Day5_assign/Program.cs
@@ -6,8 +6,25 @@ namespace Simple
     {
         static void Main(string[] args)
         {
-            P1 c1 = new P1();
-            c1.Flipcoin();
+            Console.WriteLine("Enter 1 for flipcoin_program \nEnter 2 for Leap Year\nEnter 3 for Power of 2 program\nEnter 4 for Harmonic number program\nEnter 5 for Factors program\nEnter 6 for quotient program\nEnter 7 for Swap number program\nEnter 8 for Even/odd program\nEnter 9 for Vowel/consonant program\nEnter 10 for largest among 3 numbers program ");
+            byte user_input=(byte)Convert.ToInt32(Console.ReadLine());
+            switch (user_input)
+            {
+                case 1:
+                    P1 c1 = new P1();
+                    c1.Flipcoin();
+                    break;
+                    case 2:
+                    P2 c2 = new P2();
+                    c2.leapYear();
+                    break;
+                    default: Console.WriteLine("Please enter correct number");
+                    break;
+
+            }
+                
+            
+
         }
     }
 }
